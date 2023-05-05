a4957d2f3a46be61a5b704e28f6957b114895297
stumpfi06 <69769651+stumpfi06@users.noreply.github.com>
2023-05-05 08:37:05 +0200

    Add files via upload

---
+++ MySplit/header.php
@@ -0,0 +1,13 @@
+<?php
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "<meta charset=\"utf-8\" />\n";
+echo "<title>weather-API</title>\n";
+echo"<style>
+			
+			
+		</style>";
+echo "</head>\n";
+echo "<body>\n";
+?>
