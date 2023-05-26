2bae7bd4b3b42a90773ac4f6b60b4de755b0e90d
Hieba123 <christoph.hieber@gmx.at>
2023-05-26 14:04:41 +0200

    login
    login admin geht

--- MySplit/php/login.php
+++ MySplit/php/login.php
@@ -1,21 +1,17 @@
 <?php
-function handleLoginForm()
-{
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
         // Überprüfen, ob der Name und das Passwort gesendet wurden
-        if (isset($_POST['name']) && isset($_POST['password'])) {
-            $name = $_POST['name'];
-            $password = $_POST['password'];
+        if (isset($_POST['benutzername']) && isset($_POST['passwort'])) {
+            $name = $_POST['benutzername'];
+            $password = $_POST['passwort'];
 
             
-            if ($password === 'admin' && $name=='@admin.com') {
+            if ($password === 'admin' && $name=='admin@admin.com') {
                 echo "Willkommen, $name!";
-            } else {
-                echo "Ungültiges Passwort!";
-            }
-        } else {
-            echo "Bitte geben Sie Name und Passwort ein.";
-        }
+            } 
+        } 
     }
-}
+
 ?>
