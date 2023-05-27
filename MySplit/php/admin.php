9d3c74617663aa22e265d7796134704f7bbdd700
Hieba123 <christoph.hieber@gmx.at>
2023-05-27 14:55:14 +0200

    test

---
+++ MySplit/php/admin.php
@@ -0,0 +1,33 @@
+<?php
+function generatePassword() {
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$123456789';
+    $password = '';
+    
+    $charLength = strlen($characters);
+    for ($i = 0; $i < 8; $i++) {
+        $randomIndex = mt_rand(0, $charLength - 1);
+        $password .= $characters[$randomIndex];
+    }
+    
+    return $password;
+}
+
+echo generatePassword();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
