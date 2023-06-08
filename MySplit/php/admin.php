27aeb2493b88d5338d253f84943be6935a3b3a64
Hieba123 <christoph.hieber@gmx.at>
2023-06-08 16:20:38 +0200

    admin und benutzer können auf die namen zugreifen aufpassn des textfile spinnt beim aufelona des musst bei dir sicha wieder ändern

--- MySplit/php/admin.php
+++ MySplit/php/admin.php
@@ -7,5 +7,38 @@ $var2 = $_GET['var2'];
 echo $var1;  
 echo $var2; 
 
+$users = getUsersWithIdSameProject($var2, "../daten/Benutzernamen.txt");
+
+foreach ($users as $user) {
+  echo "Name: " . $user['name'] . "<br>";
+}
+function getUsersWithIdSameProject($project, $filePath)
+{
+  $users = array();
+
+  // Datei öffnen und zeilenweise lesen
+  $file = fopen($filePath, "r");
+  if ($file) {
+    while (($line = fgets($file)) !== false) {
+      // Zeile in ein Array aufteilen
+      $userInfo = explode(":", $line);
+
+      // Überprüfen, ob das Projekt übereinstimmt und die ID 2 ist
+      if (trim($userInfo[3]) === $project && trim($userInfo[2]) === "2") {
+        // Benutzer zum Ergebnis-Array hinzufügen
+        $user = array(
+          'name' => trim($userInfo[0]),
+          'password' => trim($userInfo[1]),
+          'id' => trim($userInfo[2]),
+          'project' => trim($userInfo[3])
+        );
+        $users[] = $user;
+      }
+    }
+    fclose($file);
+  }
+
+  return $users;
+}
 
 ?>
--- MySplit/php/login.php
+++ MySplit/php/login.php
@@ -1,49 +1,49 @@
 <?php
 require_once('benutzer.php');
 //header('Location: admin.php');
-$id=array();
+$id = array();
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        
-        // Überprüfen, ob der Name und das Passwort gesendet wurden
-        if (isset($_POST['benutzername']) && isset($_POST['passwort'])) {
-            $name = $_POST['benutzername'];
-            $password = $_POST['passwort'];
-            pruefeAnmeldeinformationen($name,$password);
-           
-        } 
-       
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Überprüfen, ob der Name und das Passwort gesendet wurden
+    if (isset($_POST['benutzername']) && isset($_POST['passwort'])) {
+        $name = $_POST['benutzername'];
+        $password = $_POST['passwort'];
+        pruefeAnmeldeinformationen($name, $password);
     }
-    
-   function getData() {
-            
-            
-        $zeilen = file("../daten/Benutzernamen.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        foreach ($zeilen as $zeile) {
+}
+
+function getData()
+{
+
+
+    $zeilen = file("../daten/Benutzernamen.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($zeilen as $zeile) {
         $benutzerPasswort = explode(":", $zeile);
         $benutzername = trim($benutzerPasswort[0]);
         $passwort = trim($benutzerPasswort[1]);
-        $id[]=trim($benutzerPasswort[2]);
-        
-    
+        $id[] = trim($benutzerPasswort[2]);
+
+
         $benutzerArray[$benutzername] = $passwort;
-        }
-      
-    
-        return $benutzerArray;
     }
-    
-    //echo "no";
-    function generateBackLink() {
-        $backLink = '';
-        
-        if (isset($_SERVER['HTTP_REFERER'])) {
-            $backLink = $_SERVER['HTTP_REFERER'];
-        }
-        
-        return $backLink;
+
+
+    return $benutzerArray;
+}
+
+//echo "no";
+function generateBackLink()
+{
+    $backLink = '';
+
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        $backLink = $_SERVER['HTTP_REFERER'];
     }
-    function getIdByEmail($filename, $email)
+
+    return $backLink;
+}
+function getIdByEmail($filename, $email)
 {
     $file = fopen($filename, "r");
     if ($file) {
@@ -89,31 +89,27 @@ function getProjectnameByEmail($filename, $email)
 
     return null; // Keine ID gefunden
 }
-    function pruefeAnmeldeinformationen($benutzername, $passwort) {
-        $benutzerarray=getData();
-          // Überprüfen, ob der Benutzername im Array vorhanden ist und das Passwort übereinstimmt
-          if (isset($benutzerarray[$benutzername]) && $benutzerarray[$benutzername] === $passwort) {
-            $project = getProjectnameByEmail("../daten/Benutzernamen.txt", $benutzername);
-            $id = getIdByEmail("../daten/Benutzernamen.txt", $benutzername);
-           
-            $paramString = http_build_query(array(
+function pruefeAnmeldeinformationen($benutzername, $passwort)
+{
+    $benutzerarray = getData();
+    // Überprüfen, ob der Benutzername im Array vorhanden ist und das Passwort übereinstimmt
+    if (isset($benutzerarray[$benutzername]) && $benutzerarray[$benutzername] === $passwort) {
+        $project = getProjectnameByEmail("../daten/Benutzernamen.txt", $benutzername);
+        $id = getIdByEmail("../daten/Benutzernamen.txt", $benutzername);
+
+        $paramString = http_build_query(array(
             'var1' => $benutzername,
             'var2' => $project
-            ));
-            
-            if ($id == 1) {
-              $url = "admin.php?" . $paramString;
-                header("Location: " . $url);
-               echo $benutzername."</br>";
-               echo $project;
-            } else if ($id == 2) {
-                $url2 = "benutzer.php?" . $paramString;
-                header("Location: " . $url2);
-            }
+        ));
+
+        if ($id == 1) {
+            $url = "admin.php?" . $paramString;
+            header("Location: " . $url);
+            echo $benutzername . "</br>";
+            echo $project;
+        } else if ($id == 2) {
+            $url2 = "benutzer.php?" . $paramString;
+            header("Location: " . $url2);
+        }
     }
 }
-   
-    
-    
-
-?>
